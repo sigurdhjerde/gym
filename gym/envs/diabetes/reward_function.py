c1eb2f63ae4d2eea9cdb9260ac3e0e0d703226e0
Jonas <jonas.n.myhre@uit.no>
2018-08-17 14:02:29 +0200

    hovorka interval is now adaptive part 2

--- gym/envs/diabetes/reward_function.py
+++ gym/envs/diabetes/reward_function.py
@@ -15,6 +15,19 @@ def calculate_reward(blood_glucose_level, reward_flag='absolute', bg_ref=90, act
         else:
             reward = 0
 
+    elif reward_flag == 'binary_tight':
+        ''' Tighter version of the binary reward function,
+        the bounds are [-5, 5] around the optimal rate.
+        '''
+        low_bg = 85
+        high_bg = 95
+
+        if np.max(blood_glucose_level) < high_bg and np.min(blood_glucose_level) > low_bg:
+            reward = 1
+        else:
+            reward = 0
+
+
     elif reward_flag == 'squared':
         ''' Squared cost function '''
 
@@ -32,8 +45,8 @@ def calculate_reward(blood_glucose_level, reward_flag='absolute', bg_ref=90, act
             action = [0, 0]
 
         # Parameters
-        alpha = 1
-        beta = 1
+        alpha = .7
+        beta = 1 - alpha
 
         reward = - alpha*(abs(blood_glucose_level - bg_ref)) - beta * (abs(action[1]-action[0]))
 
