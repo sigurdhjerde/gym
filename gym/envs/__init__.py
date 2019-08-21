317bbf6d9339cf66de4370d19aa1c57ef63af5a8
Jonas <jonas.n.myhre@uit.no>
2019-08-21 14:27:21 +0200

    anas patient added

--- gym/envs/__init__.py
+++ gym/envs/__init__.py
@@ -42,6 +42,15 @@ register(
         timestep_limit = 72,
         max_episode_steps= 72        
         )
+# Hovorka model with Anas' 100 patient params
+register(
+        id = 'AnasPatient-v0',
+        entry_point = 'gym.envs.diabetes.anas_patient:AnasPatient',
+        # timestep_limit = 48,
+        # max_episode_steps= 48
+        timestep_limit = 72,
+        max_episode_steps= 72        
+        )
 # ================================================
 # Cambridge model
 # ================================================
