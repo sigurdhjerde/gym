31410f183ecf60285c5d999caaf9d298dfe39cd4
Jonas <jonas.n.myhre@uit.no>
2019-09-23 16:40:48 +0200

    test environment updated

--- gym/envs/diabetes/test_env.py
+++ gym/envs/diabetes/test_env.py
@@ -1,53 +1,24 @@
 import numpy as np
 import gym
-# import seaborn as sns
-# sns.set()
+import seaborn as sns
+sns.set()
 
-from pylab import plot, figure, title, show, ion, legend
+from pylab import plot, figure, show
 
-# env = gym.make('HovorkaGaussian-v0')
-# env = gym.make('HovorkaGaussianInsulin-v0')
-# env = gym.make('HovorkaBinary-v0')
-# env = gym.make('HovorkaAbsolute-v0')
 env = gym.make('HovorkaCambridge-v0')
-
-
-# basal = 0
-# env.env.reset_basal_manually = 6.43
 
 reward = []
 bg = []
 cgm = []
 
-# env.env.reset_basal_manually = 6.43
-# env.env.bolus = 8.3
+env.env.reset_basal_manually = env.env.init_basal_optimal
 env.reset()
 
-for i in range(48):
+for i in range(72):
 
     # Step for the minimal/hovorka model
-    # s, r, d, i = env.step(np.array([6.43]))
-    s, r, d, i = env.step(np.array([11]))
-    # s, r, d, i = env.step(np.array([0]))
-
-    # bg.append(env.env.simulation_state[4])
-    # cgm.append(env.env.simulation_state[-1] * env.env.P[12])
-    # reward.append(r)
-    # print(r)
-
-    # Step for the discrete Hovorka incremental
-    # env.step(2)
+    s, r, d, i = env.step(np.array([env.env.init_basal_optimal]))
 
 
 
-# env.render()
-figure()
-plot(env.env.bg_history)
-# plot(bg)
-# plot(cgm)
-# legend(['bg', 'cgm'])
-# title('bg and cgm')
-# ion()
-# show()
-plot(env.env.bg_history)
-show()
+env.render()
