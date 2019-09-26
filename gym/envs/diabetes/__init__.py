9d72d636587665c402d1027d52d4ffeab2c4cd46
Sigurd Hjerde <43098627+sigurdhjerde@users.noreply.github.com>
2019-09-26 14:22:52 +0200

    Importing new Hovorka model

--- gym/envs/diabetes/__init__.py
+++ gym/envs/diabetes/__init__.py
@@ -1,2 +1,3 @@
 # 30 minute interval with meals
 from gym.envs.diabetes.hovorka_cambridge import HovorkaCambridgeBase
+from gym.envs.diabetes.hovorka_cambridge_sigurd import HovorkaCambridgeBase_Sigurd
